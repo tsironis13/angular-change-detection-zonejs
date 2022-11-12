c897728a4c724358e638c51e166091c7d58adc5c
Matthieu Riegler <kyro38@gmail.com>
2022-11-12 11:36:48 +0100

    broken ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit, ViewChild, ViewContainerRef } from "@angular/core";
+import { UpperCasePipe } from "@angular/common";
+import {
+  Component,
+  EnvironmentInjector,
+  inject,
+  Injectable,
+  Injector,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+@Injectable()
+class Foo {
+  foo = "foo";
+}
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   standalone: true,
+  imports: [FormsModule, UpperCasePipe],
 })
 export class AppComponent implements OnInit {
   @ViewChild("container", { read: ViewContainerRef, static: true })
   container!: ViewContainerRef;
 
+  test = "test123";
+  injector = inject(Injector);
+  environement = inject(EnvironmentInjector);
+
   ngOnInit() {
     const component = Component({
-      template: "test no",
+      selector: "ddddd",
+      template: 'test {{dynamic | uppercase }} but no input => <input [(ngModel)]="dynamic"/> -- {{foo.foo}}',
       styles: [":host {color: red; padding-left: 8px; display:block; }"],
-    })(class {});
+      imports: [FormsModule, UpperCasePipe],
+      providers: [Foo],
+    })(
+      class Azerty {
+        dynamic = "dynamic";
+        foo = inject(Foo); // OK
+      }
+    );
     this.container.createComponent(component);
   }
 }
